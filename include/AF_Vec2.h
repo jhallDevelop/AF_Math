0af60f7dba80d74437720353aa3ace6a4e90fe15
jhall.develop <jhall.develop@gmail.com>
2024-10-30 20:32:34 +1000

    EDIT: updated vector

--- include/AF_Vec2.h
+++ include/AF_Vec2.h
@@ -32,7 +32,7 @@ extern "C" {
     Function for adding two 2D vectors.
     ====================
     */
-    static inline Vec2 Vec2ADD(Vec2 v1, Vec2 v2)
+    static inline Vec2 Vec2_ADD(Vec2 v1, Vec2 v2)
     {
         Vec2 result = {0,0};
         result.x = v1.x + v2.x;
--- include/AF_Vec3.h
+++ include/AF_Vec3.h
@@ -275,6 +275,8 @@ extern "C" {
         return fabs((_v1.x * (_v2.y - _v3.y) + _v2.x * (_v3.y - _v1.y) + _v3.x * (_v1.y - _v2.y)) / 2);
     }
 
+
+
 #ifdef __cplusplus
 }
 #endif
